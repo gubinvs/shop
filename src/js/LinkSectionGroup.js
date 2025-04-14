a32254cd695fa63d74b3b324f3e7a0ad2a3b4665
master <gubinvs@gmail.com>
2025-04-14 19:52:00 +0300

    x

--- src/js/LinkSectionGroup.js
+++ src/js/LinkSectionGroup.js
@@ -3,13 +3,12 @@
 
 export const chapterMa = "Модульные автоматы";
 export const chapterBp = "Блоки питания";
-export const chapterK = "Клеммы и маркировка";
+export const chapterK = "Клеммы и провода";
 export const chapterSch = "Типовые решения";
 export const chapterPlk = "Программируемые контроллеры";
 export const chapterMplk = "Модули расширения";
 export const chapterSl = "Сигнальные лампы";
 export const chapterUps = "Устройства плавного пуска";
-export const chapterСable = "Кабель и провод";
 
 export const OpenSection = (param) => {
     window.location.href = "/CatalogSection/?chapter=" + param;
