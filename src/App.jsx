f3367b66d49f56c83ca6f7e42998d40cc154ffdd
master <gubinvs@gmail.com>
2025-08-31 21:32:35 +0300

    ы

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function isTokenValid(token) {
     const currentTime = Math.floor(Date.now() / 1000); // Текущее время в секундах
     return decoded.exp > currentTime; // Проверяем, не истек ли токен
   } catch (error) {
-    console.error("Ошибка проверки токена:", error);
+      console.error("Ошибка проверки токена:", error);
     return false; // Если токен невалиден или не может быть декодирован
   }
 }
