a69fdacbd0e1f6d13719030cb9eb2babca8970ba
gubinvs@gmail.com <gubinvs@gmail.com>
2025-10-31 11:41:00 +0300

    r

---
+++ src/js/nomenclatureStore.js
@@ -0,0 +1,29 @@
+import ApiUrl from "./ApiUrl";
+/// Скрипт загружает в фоновом режиме все данные о товарах, для дальнейшего использования в приложении,
+/// компоненты уже фильтруют эти данные у себя сами
+
+
+// nomenclatureStore.js
+let nomenclature = [];
+let loaded = false;
+
+export async function loadNomenclature() {
+  if (loaded) return nomenclature;
+  const response = await fetch(ApiUrl + "/api/nomenclature");
+  const data = await response.json();
+  nomenclature = data;
+  loaded = true;
+  console.log("Номенклатура загружена:", data.length, "позиций");
+  return nomenclature;
+}
+
+export function getNomenclature() {
+  return nomenclature;
+}
+
+export function isLoaded() {
+  return loaded;
+}
+
+
+/// Реализация в компоненте import { loadNomenclature, isLoaded } from "./js/nomenclatureStore";
