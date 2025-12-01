ed7188ffe839df9412b25eb27e809bb63689d54c
gubinvs@gmail.com <gubinvs@gmail.com>
2025-12-01 16:15:28 +0300

    w

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import HeaderGuest from '../Header/HeaderGuest.jsx';
 import Header from '../Header/Header.jsx';
 import './Home.css';
-import HomeMainSection from "./HomeMainSection.jsx";
+// import HomeMainSection from "./HomeMainSection.jsx";
 import DirectoryGroups from "./DirectoryGroups.jsx";
 import CardComponetGroop from "../CardComponetGroop/CardComponetGroop.jsx";
 import Footer from "../Footer/Footer.jsx";
@@ -17,7 +17,7 @@ const Home = () => {
         {/* Если не авторизован выводим шапку для гостя */}
             {/* {isAuthenticated ? <Header /> : <HeaderGuest />}  */}
             <Header />
-            <HomeMainSection />
+            {/* <HomeMainSection /> */}
             <DirectoryGroups />
             <CardComponetGroop h2="" api={"/api/Bestsellers"} />
             <Footer />
