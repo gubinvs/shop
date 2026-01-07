d6327409b76566651a4af0ce5f393bf4ec2e5e9c
master <gubinvs@gmail.com>
2026-01-07 16:06:48 +0300

    f

--- src/AdminPanel/Warehouse/Coming.jsx
+++ src/AdminPanel/Warehouse/Coming.jsx
@@ -8,8 +8,18 @@ const Coming = ({itemComponent}) => {
         <>
             <div className="coming-section__container">
                 <h2 className="coming-section__title">Оприходование товара:</h2>
-                <input type="text" className="coming-section__search" placeholder="Поиск по артикулу" />
-                <div className="cs-search-result"></div>
+                <div className="coming-section__search-block">
+                    <input type="text" className="coming-section__search" placeholder="Поиск по артикулу" />
+                    <div className="cs-search-result"></div> 
+                </div>
+                <div className="coming-section__add-component-form">
+                    <input type="text" className="coming-section__article" id="" value="TM3AI8" readOnly />
+                    <input type="text" className="coming-section__name" id="" value="Наименование комплектующих" readOnly />
+                    <input type="number" className="coming-section__number" id="" placeholder="0" min="0"/>
+                    <input type="text" className="coming-section__discription" id="" placeholder="Примечание" />
+                    <button className="coming-section__button">Записать</button>
+                </div>
+                
             </div>
         </>
     );
