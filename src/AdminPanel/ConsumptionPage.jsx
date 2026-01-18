5afc3a6398a66f8628698d35bcae7007b84f8ccd
master <gubinvs@gmail.com>
2026-01-18 19:13:54 +0300

    ы

--- src/AdminPanel/ConsumptionPage.jsx
+++ src/AdminPanel/ConsumptionPage.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import ApiUrl from "../js/ApiUrl.js";
 import "./adminPanel.css";
 import "./mainSectionAdminPanel.css";
 import NavigationBar from "../AdminPanel/NavigationBar/NavigationBar.jsx";
@@ -8,6 +10,45 @@ import {item} from "./item.js";
 
 /// Страница данных о реализации товаров со склада
 const ConsumptionPage =()=> {
+
+    const [docList, setDocList] = useState([]);
+    // Форматирует строковую дату в читабельный вид
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const response = await fetch(
+                    `${ApiUrl}/api/ReturnConsumptionComponent/MySuperToken123`
+                );
+
+                if (!response.ok) {
+                    throw new Error("Ошибка запроса: " + response.status);
+                }
+
+                const data = await response.json();
+
+                const formattedData = data.map(item => ({
+                    id: item.id,
+                    guid: item.guidIdComponent,
+                    vendorCode: item.vendorCode,
+                    nameComponent: item.nameComponent,
+                    quantity: item.quantityGoods,
+                    price: item.itemPrice,
+                    noteDiscription: item.noteDiscription,
+                    dateSave: item.dataRecord
+                }))
+                .reverse();
+
+                setDocList(formattedData);
+
+            } catch (err) {
+                console.error("Ошибка загрузки номенклатуры:", err);
+            }
+        };
+
+        loadData();
+    }, []);
+
     return (
         <>
            <div className="admin-panel__container">
@@ -15,7 +56,7 @@ const ConsumptionPage =()=> {
                 <div className="main-section-admin-panel">
                     <Coming itemComponent={item} title={"Реализация товара"} addDataFunction={clearComingComponent} />
                     <br/><br/><br/>
-                    <ComingResult title={"Перечень операций реализации:"}/>
+                    <ComingResult title={"Перечень операций реализации:"} list={docList}/>
                 </div>
             </div>
         </>
