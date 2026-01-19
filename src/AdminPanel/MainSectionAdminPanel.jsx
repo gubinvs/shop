fab479ca7572e39a082238aa4dfff5da566ba3e9
gubinvs@gmail.com <gubinvs@gmail.com>
2026-01-19 16:43:54 +0300

    h

--- src/AdminPanel/MainSectionAdminPanel.jsx
+++ src/AdminPanel/MainSectionAdminPanel.jsx
@@ -9,9 +9,9 @@ const MainSectionAdminPanel = () => {
         <>
             <div className="main-section-admin-panel">            
                 <div className="admin-mailer__container">
-                    <AdminMailer title={"Рассылка по проверенной базе:"} urlRegust={"http://31.129.97.48:1100/api/Upload/upload-excel"}/>
+                    <AdminMailer title={"Рассылка от gmail.com:"} urlRegust={"http://31.129.97.48:1100/api/Upload/upload-excel"}/>
                     <br/><br/>
-                    <AdminMailer title={"Рассылка по базе на отсев:"} urlRegust={"http://31.129.97.48:1110/api/Upload/upload-excel"}/>
+                    <AdminMailer title={"Рассылка от mail.ru:"} urlRegust={"http://31.129.97.48:1110/api/Upload/upload-excel"}/>
                 </div>
             </div>
         </>
