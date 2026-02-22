a581268da6f838dfe5f432a515c8fc41f360a04d
master <gubinvs@gmail.com>
2026-02-22 18:31:59 +0300

    s

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-// import HeaderGuest from '../Header/HeaderGuest.jsx';
 import Header from '../Header/Header.jsx';
-import './Home.css';
-// import HomeMainSection from "./HomeMainSection.jsx";
-import DirectoryGroups from "./DirectoryGroups.jsx";
+import './home.css';
 import NewDirectoryGroups from "./NewDirectoryGroups.jsx";
 import CardComponetGroop from "../CardComponetGroop/CardComponetGroop.jsx";
 import Footer from "../Footer/Footer.jsx";
@@ -15,13 +12,39 @@ const Home = () => {
 
     return (
         <>
-        {/* Если не авторизован выводим шапку для гостя */}
-            {/* {isAuthenticated ? <Header /> : <HeaderGuest />}  */}
             <Header />
-            {/* <HomeMainSection /> */}
-            {/* <DirectoryGroups /> */}
             <NewDirectoryGroups />
-            <CardComponetGroop h2="" api={"/api/Bestsellers"} />
+            <CardComponetGroop h2="Популярные товары" api={"/api/Bestsellers"} />
+            <div className="home-delivery-section">
+                <div className="container home-delivery-section__container">
+                    <h2 className='home-delivery-section__title'>Способы доставки</h2>
+                    <img 
+                        className='home-delivery-section__images'
+                        src="/images/delivry-man__1920.png" 
+                        alt="Молодой человек с желтой сумкой доставки" 
+                    />
+                    <div className="home-delivery-section__icon-block">
+                        <img 
+                            src="/images/del-line-logo__1920.png" 
+                            alt="Логотип деловых линий" 
+                            className="home-delivery-section__icon" 
+                        />
+                        <img 
+                            src="/images/sdeck-logo__1920.png" 
+                            alt="Логотип деловых линий" 
+                            className="home-delivery-section__icon" 
+                        />
+                        <img 
+                            src="/images/ozon-logo__1920.png" 
+                            alt="Логотип деловых линий" 
+                            className="home-delivery-section__icon" 
+                        />
+                    </div>
+                    <div className="home-delivery-section__discr">
+                        Мы сотрудничаем с надежными транспортными компаниями. Сроки и стоимость перевозки зависят от региона и рассчитываются в индивидуальном порядке.
+                    </div>
+                </div>
+            </div>
             <Footer />
         </> 
     );
