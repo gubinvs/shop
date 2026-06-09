a0251d67daddb651532ee041642ed9d843926a10
master <gubinvs@gmail.com>
2026-06-09 19:58:57 +0300

    a

--- src/js/ApiUrl.js
+++ src/js/ApiUrl.js
@@ -2,8 +2,8 @@
 
 // Переменная с адресом сервера API
 // const ApiUrl =  'http://localhost:8080';
-// const ApiUrl =  'https://api.encomponent.ru';
-const ApiUrl =  'http://193.22.244.229/api';
+const ApiUrl =  'https://api.encomponent.ru';
+// const ApiUrl =  'http://193.22.244.229/api';
 
 
 export default ApiUrl;
