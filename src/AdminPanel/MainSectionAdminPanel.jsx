cea74a9ad7ade76a9c719c247f6e6d162af3450b
gubinvs@gmail.com <gubinvs@gmail.com>
2026-08-13 12:59:20 +0300

    t

--- src/AdminPanel/MainSectionAdminPanel.jsx
+++ src/AdminPanel/MainSectionAdminPanel.jsx
@@ -18,12 +18,6 @@ const MainSectionAdminPanel = () => {
                         <GeneralPurchaseSection title="Сумма в товаре:" controllerName="ReturnCostPriceOfGoods" />
                         <GeneralPurchaseSection title="Продается на сумму:" controllerName="TotalSellingPrice" />
                     </div>
-                    <br/><br/>
-                    <AdminMailer title={"Рассылка от gmail.com:"} urlRegust={"http://31.129.97.48:1100/api/Upload/upload-excel"}/>
-                    <br/><br/>
-                    <AdminMailer title={"Рассылка от mail.ru:"} urlRegust={"http://31.129.97.48:1110/api/Upload/upload-excel"}/>
-                    <br/><br/>
-                    <UpdateQuantityKeaz />
                 </div>
             </div>
         </>
