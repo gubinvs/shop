6667a95ed3875d13d5072e076cbecee117068667
gubinvs@gmail.com <gubinvs@gmail.com>
2026-08-26 15:22:54 +0300

    e

--- src/CatalogSection/CatalogSection.jsx
+++ src/CatalogSection/CatalogSection.jsx
@@ -3,14 +3,11 @@ import "./catalogSection.css";
 import Header from '../Header/Header';
 import HeaderGuest from "../Header/HeaderGuest.jsx";
 import {jsonCartTest} from "../js/jsonCartTest.js";
-import CardComponetGroopLocalData from "../CardComponetGroop/CardComponetGroopLocalData.jsx";
-import Footer from '../Footer/Footer.jsx';
 import GroupOfCards from '../GroupOfCards/GroupOfCards.jsx';
 
 const CatalogSection = ({nomenclature}) => {
   // Определяем название каталога
   const chapter = new URLSearchParams(window.location.search).get("chapter");
-  const newChapter = chapter === "Модульные автоматы" ? "Модульное оборудование" : chapter;
 
   // Проверка авторизации пользователя
   const isAuthenticated = localStorage.getItem('token') !== null;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Header from '../Header/Header.jsx';
 import HeaderGuest from '../Header/HeaderGuest.jsx'
 import './home.css';
-import NewDirectoryGroups from "./NewDirectoryGroups.jsx";
-import CardComponetGroop from "../CardComponetGroop/CardComponetGroop.jsx";
-import Footer from "../Footer/Footer.jsx";
-import CardComponent from '../CardComponent/CardComponent.jsx';
 import GroupOfCards from '../GroupOfCards/GroupOfCards.jsx';
 import NewDirectoryGroupsMin from './NewDirectoryGroupsMin.jsx';
 import {jsonCartTest} from "../js/jsonCartTest.js";
