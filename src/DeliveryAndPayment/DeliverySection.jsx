8813bcae0a3a0a12a9c8ec4116a73f21d81eed7e
gubinvs@gmail.com <gubinvs@gmail.com>
2026-09-03 14:39:42 +0300

    t

--- src/DeliveryAndPayment/DeliverySection.jsx
+++ src/DeliveryAndPayment/DeliverySection.jsx
@@ -15,50 +15,25 @@ const DeliverySection = () => {
                         <div className="home-delivery-section__discr-block">
                             <h2 className='home-delivery-section__title'>Способы доставки</h2>
                             <div className="home-delivery-section__icon-block">
-                                <picture>
-                                    <source srcset="/images/img-delivery-section/del-line-logo__1280.png" media="(min-width: 1281px)" />
-                                    <source srcset="/images/img-delivery-section/del-line-logo__1024.png" media="(min-width: 1025px)" />
-                                    <source srcset="/images/img-delivery-section/del-line-logo__960.png" media="(min-width: 961px)" />
-                                    <source srcset="/images/img-delivery-section/del-line-logo__800.png" media="(min-width: 801px)" />
-                                    <source srcset="/images/img-delivery-section/del-line-logo__640.png" media="(min-width: 641px)" />
-                                    <source srcset="/images/img-delivery-section/del-line-logo__480.png" media="(min-width: 481px)" />
-                    
+        
                                     <img 
                                         src="/images/img-delivery-section/del-line-logo__1920.png" 
                                         alt="Логотип деловых линий" 
                                         className="home-delivery-section__icon" 
                                     />
-                                </picture>
-                                
-                                <picture>
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__1280.png" media="(min-width: 1281px)" />
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__1024.png" media="(min-width: 1025px)" />
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__960.png" media="(min-width: 961px)" />
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__800.png" media="(min-width: 801px)" />
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__640.png" media="(min-width: 641px)" />
-                                    <source srcset="/images/img-delivery-section/sdeck-logo__480.png" media="(min-width: 481px)" />
-                        
+                               
                                     <img 
                                         src="/images/img-delivery-section/sdeck-logo__1920.png" 
                                         alt="Логотип деловых линий" 
                                         className="home-delivery-section__icon" 
                                     />
-                                </picture>
                                 
-                                <picture>
-                                    <source srcset="/images/img-delivery-section/ozon-logo__1280.png" media="(min-width: 1281px)" />
-                                    <source srcset="/images/img-delivery-section/ozon-logo__1024.png" media="(min-width: 1025px)" />
-                                    <source srcset="/images/img-delivery-section/ozon-logo__960.png" media="(min-width: 961px)" />
-                                    <source srcset="/images/img-delivery-section/ozon-logo__800.png" media="(min-width: 801px)" />
-                                    <source srcset="/images/img-delivery-section/ozon-logo__640.png" media="(min-width: 641px)" />
-                                    <source srcset="/images/img-delivery-section/ozon-logo__480.png" media="(min-width: 481px)" />
-                                    
                                     <img 
                                         src="/images/img-delivery-section/ozon-logo__1920.png" 
                                         alt="Логотип деловых линий" 
                                         className="home-delivery-section__icon" 
                                     />
-                                </picture>
+                               
                             </div>
                             <div className="home-delivery-section__discr">
                                 Мы доставляем заказы через проверенные транспортные компании, 
