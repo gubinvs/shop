35020babc1b0193b084880e61235020cc49cf299
master <gubinvs@gmail.com>
2026-09-23 21:34:07 +0300

    а

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -5,7 +5,75 @@ import "./contactPage.css";
 const ContactPage = () => {
     return(
         <>
-        
+        <section class="egrul-table-section">
+            <div class="container egrul-table-section__container">
+                <div class="egrul-table-section__file">
+                    <a href="https://file.ec-market.ru/files/ipgubincard.pdf">
+                        <img class="icon-file" src="/images/pdf-icon.png" alt="Иконка файла pdf" />
+                    </a>
+                </div>
+                <h2 class="egrul-table-section__title">Корпоративная информация</h2>
+                <table class="egrul-table-section__table">
+                    <tr>
+                        <td class="egrul-table-section__td">Полное наименование</td>
+                        <td class="egrul-table-section__td">Индивидуальный предприниматель Губин Владимир Сергеевич</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Сокращенное наименование</td>
+                        <td class="egrul-table-section__td">ИП Губин Владимир Сергеевич</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Коммерческое обозначение (бренд)</td>
+                        <td class="egrul-table-section__td">Компоненты энергии</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">ИНН</td>
+                        <td class="egrul-table-section__td">590302376610</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">ОГРНИП</td>
+                        <td class="egrul-table-section__td">325784700338340</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Адрес местонахождения</td>
+                        <td class="egrul-table-section__td">197371, г. Санкт-Петербург, ул. Парашютная, д. 34, стр. 1, кв. 851</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">ОКВЭД</td>
+                        <td class="egrul-table-section__td">47.91.2, 52.21.1, 62.01, 63.11.1, 70.22, 82.99</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Расчетный счет №</td>
+                        <td class="egrul-table-section__td">40802 810 3 5571 0027166</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Корреспондентский счет №</td>
+                        <td class="egrul-table-section__td">30101 810 5 0000 0000653</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Наименование банка</td>
+                        <td class="egrul-table-section__td">ПАО СБЕРБАНК</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">БИК</td>
+                        <td class="egrul-table-section__td">044030653</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">Телефон</td>
+                        <td class="egrul-table-section__td">+7 (812) 921-59-71</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">E-mail</td>
+                        <td class="egrul-table-section__td">gubinvs@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td class="egrul-table-section__td">ЭДО ID</td>
+                        <td class="egrul-table-section__td">2BE5ac19fd465c94c5884077afd69cef7c1</td>
+                    </tr>
+                </table>
+            </div>
+
+        </section>
         </>
     );
 }
